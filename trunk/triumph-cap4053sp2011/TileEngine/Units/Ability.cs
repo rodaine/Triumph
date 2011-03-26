99bb9852c029357c08fa946cf07d7c0bf6ad25a4
rodaine <rodaine@4601a31f-7dae-600b-f67e-0e9fd6964f0b>
2011-03-26 19:59:17 +0000

    Moved unit classes into TileEditor namespace

---
+++ trunk/triumph-cap4053sp2011/TileEngine/Units/Ability.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TileEngine
+{
+    class Ability
+    {
+        #region ability fields
+
+        int apCost;
+        int attackRange;
+        int abilityType;
+        int abilityAmount;
+        String name;
+        String description;
+
+        #endregion
+
+        #region constructors
+
+        public Ability(String name, int abilityType, int abilityAmount, int apCost, int attackRange, String description)
+        {
+            this.name = name;
+            this.apCost = apCost;
+            this.attackRange = attackRange;
+            this.description = description;
+
+            this.abilityType = abilityType;
+            this.abilityAmount = abilityAmount;
+
+        }
+
+        public Ability(String name, int abilityType, int abilityAmount, int apCost, int attackRange)
+        {
+            this.name = name;
+            this.apCost = apCost;
+            this.attackRange = attackRange;
+            this.description = "No description provided.";
+
+            this.abilityType = abilityType;
+            this.abilityAmount = abilityAmount;
+        }
+
+        public Ability()
+        {
+            name = "";
+            apCost = 0;
+            attackRange = 0;
+            description = "";
+
+            abilityType = -1;
+            abilityAmount = 0;
+        }
+
+        #endregion
+
+        #region get methods
+
+        public String getName() { return name; }
+        public String getDescription() { return description; }
+
+        public int getAPCost() { return apCost; }
+        public int getAttackRange() { return attackRange; }
+        public int getAbilityType() { return abilityType; }
+        public int getAbilityAmount() { return abilityAmount; }
+
+        #endregion
+    }
+}
---
+++ trunk/triumph-cap4053sp2011/TileEngine/Units/Buffs.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TileEngine
+{
+     class TriggerObject
+    {
+        #region triggerobject fields
+
+        String name;
+        String description;
+        int turnDuration;
+        int objectAmount;
+        int objectType;         
+        /*
+         * heal = 1
+         * damage = 2
+         * stun = 3
+         * incMP = 4
+         * decMP = 5
+         * incAP = 6
+         * decAP = 7
+         * 
+         * */
+        Boolean objectFlip;
+
+        #endregion
+
+        #region constructors
+
+        public TriggerObject(String name, int objectType, int objectAmount, int turnDuration, Boolean objectFlip, String description)
+        {
+            this.name = name;
+            this.objectType = objectType;
+            this.objectAmount = objectAmount;
+            this.turnDuration = turnDuration;
+            this.objectFlip = objectFlip;
+            this.description = description;
+        }
+
+        public TriggerObject(String name, int objectType, int objectAmount, int turnDuration, Boolean objectFlip)
+        {
+            this.name = name;
+            this.objectType = objectType;
+            this.objectAmount = objectAmount;
+            this.turnDuration = turnDuration;
+            this.objectFlip = objectFlip;
+            description = "No description provided.";
+        }
+
+        public TriggerObject()
+        {
+            name = "";
+            objectType = -1;
+            objectAmount = 0;
+            turnDuration = 0;
+            objectFlip = false;
+            description = "";
+        }
+
+        #endregion
+
+        #region get methods
+        
+        public int getObjectType() { return objectType; }
+        public int getObjectAmount() { return objectAmount; }
+        public int getTurnDuration() { return turnDuration; }
+        public String getName() { return name; }
+        public String getDescription() { return description; }
+        public Boolean getObjectFlip() { return objectFlip; }
+
+        #endregion
+
+        #region methods
+
+        public void decTurn()
+        {
+            turnDuration--;
+        }
+
+        #endregion
+
+    }
+}
+}
