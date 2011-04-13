61ba941e2c65cabf1f05c4361b2d6afc7249a0b7
Boyd.Thomas3@gmail.com <Boyd.Thomas3@gmail.com@4601a31f-7dae-600b-f67e-0e9fd6964f0b>
2011-04-13 23:50:29 +0000

    Sup y'alls I got some AI changes... Abilities are causing me some headaches and I haven't put the "thinking" delays back in yet but I wanted to upload this before the meeting. Sorry I couldn't make it tonight! I'm going to make the AI all smart and such tomorrow night after my database exam. Let me know if there's anything else you want me to work on by Monday.

--- trunk/triumph-cap4053sp2011/TileEngine/AI.cs
+++ trunk/triumph-cap4053sp2011/TileEngine/AI.cs
@@ -8,13 +8,14 @@ namespace TileEngine
 {
     public class AI
     {
+        private enum AIState { startTurn, chooseTarget, chooseMoveTowards, chooseMoveAway, chooseAttack, moveToTarget, attackTarget, endTurn };
+
         #region data members
         private bool _startNextTurn;
-        private BaseUnit _target;
+        private BaseUnit _targetUnit;
         private Point _targetPoint;
-        private bool _hasAttacked;
-        private bool _hasMoved;
-        private int _thinkDelay;
+        private AIState _myState;
+        private int[] _APToRange;
         #endregion
 
         #region constructor
@@ -32,10 +33,8 @@ namespace TileEngine
         private void startNextTurn()
         {
             _startNextTurn = true;
-            _target = null;
-            _hasAttacked = false;
-            _hasMoved = false;
-            _thinkDelay = 0;
+            _targetUnit = null;
+            _myState = AIState.startTurn;
         }
 
         /// <summary>
@@ -45,41 +44,88 @@ namespace TileEngine
         /// <param name="map">The game map</param>
         /// <param name="testUnits">All the units on the map</param>
         /// <returns>True if a target was found, false otherwise</returns>
-        private bool chooseTarget(BaseUnit currentUnit, TileMap map, BaseUnit[] testUnits)
+        private bool chooseMoveIntoRange(BaseUnit currentUnit, TileMap map, BaseUnit[] testUnits)
         {
-            _target = null;
             Faction myFaction = currentUnit.faction;
             _targetPoint = currentUnit.position;
             int minDist = Int32.MaxValue;
 
+            HashSet<Point> possibleTargets = new HashSet<Point>();
+
+            //Generate list of 
             foreach (BaseUnit bu in testUnits)
             {
                 if (!bu.isDead && myFaction != bu.faction)
                 {
                     Point targetLoc = bu.position;
-                    List<Point> possibleTargets = new List<Point>();
+                    
+                    //TODO fix ranges
                     possibleTargets.Add(new Point(targetLoc.X + 1, targetLoc.Y));
                     possibleTargets.Add(new Point(targetLoc.X - 1, targetLoc.Y));
                     possibleTargets.Add(new Point(targetLoc.X, targetLoc.Y + 1));
                     possibleTargets.Add(new Point(targetLoc.X, targetLoc.Y - 1));
-                    foreach (Point p in possibleTargets)
-                    {
+                }
+            }
 
-                        if (map.isEmpty(p) || currentUnit.position == p)
-                        {
-                            int dist = map.getPath(currentUnit, p, new List<Point>()).Count;
-                            if (dist < minDist)
-                            {
-                                _target = bu;
-                                minDist = dist;
-                                _targetPoint = p;
-                            }
-                        }
+            foreach (Point p in possibleTargets)
+            {
+
+                if (map.isEmpty(p) || currentUnit.position == p)
+                {
+                    int dist = map.getPath(currentUnit, p, new List<Point>()).Count;
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        _targetPoint = p;
                     }
                 }
             }
-            return (_target != null);
+
+            return (_targetPoint.Equals(currentUnit.position));
         }
+
+
+        //TODO fix this
+        /// <summary>
+        /// Chooses who to attack from the set of units in attack range
+        /// </summary>
+        /// <param name="currentUnit"></param>
+        /// <param name="map"></param>
+        /// <param name="testUnits"></param>
+        /// <returns></returns>
+        private bool chooseAttackTarget(BaseUnit currentUnit, TileMap map, BaseUnit[] testUnits)
+        {
+            _targetUnit = null;
+            Faction myFaction = currentUnit.faction;
+
+            HashSet<BaseUnit> possibleTargets = new HashSet<BaseUnit>();
+
+            //Generate list of 
+            foreach (BaseUnit bu in testUnits)
+            {
+                if (!bu.isDead && myFaction != bu.faction && currentUnit.withinRange(bu))
+                {
+                    _targetUnit = bu;
+                    break;
+                }
+            }
+
+            
+            return (_targetUnit != null);
+        }
+
+        /// <summary>
+        /// Figures out which ability is the best to be used from this spot.
+        /// </summary>
+        /// <param name="currentUnit"></param>
+        /// <param name="map"></param>
+        /// <param name="testUnits"></param>
+        /// <returns></returns>
+        private Ability determineMostPrudentAbility(BaseUnit currentUnit, TileMap map, BaseUnit[] testUnits)
+        {
+            return null;
+        }
+
         #endregion
 
         #region public methods
@@ -96,84 +142,174 @@ namespace TileEngine
         /// <param name="camera">The current camera</param>
         public void update(GameTime gameTime, BaseUnit currentUnit, Cursor cursor, TileMap map, int viewWidth, int viewHeight, BaseUnit[] testUnits, Camera camera)
         {
-            if (_startNextTurn)
+            if (!currentUnit.isWalking && !currentUnit.isAttacking)
             {
-                if (chooseTarget(currentUnit, map, testUnits))
+                switch (_myState)
                 {
-                    _startNextTurn = false;
-                    _thinkDelay = 50;
-                }
-                else
-                {
-                    this.startNextTurn();
-                    currentUnit.isDone = true;
-                }
-            }
-            else
-            {
-                if (!currentUnit.isWalking && !currentUnit.isAttacking)
-                {
-                    //System.Console.Error.WriteLine(currentUnit.name + " is targeting " + target.name);
-                    if (_thinkDelay > 0)
-                    {
-                        --_thinkDelay;
-                    }
-                    else
-                    {
-                        if (_hasAttacked)
+                    #region startTurn
+                    case AIState.startTurn:
                         {
-                            //AI currently too dumb to do anything after attacking
-                            this.startNextTurn();
-                            currentUnit.isDone = true;
-                        }
-                        else if (!_hasMoved && currentUnit.MP > 0 && !currentUnit.withinRange(_target))
-                        {
-
-                            Stack<Point> path = map.getPath(currentUnit, _targetPoint, new List<Point>());
-                            Stack<Point> myPath = new Stack<Point>();
-                            int count = currentUnit.MP;
-                            while (path.Count > 0 && count >= 0)
+                            //TODO napsack problem to figure out best combination of abilities
+                            /*
+                            _APToRange = new int[currentUnit.AP + 1];
+                            for (int i = 0; i <= currentUnit.AP; ++i)
                             {
-                                --count;
-                                myPath.Push(path.Pop());
-                            }
 
-                            if (myPath.Count > 0)
+                            }
+                             */
+
+                            if (currentUnit.AP > 0)
                             {
-                                Point walkTo = myPath.Pop();
-                                while (myPath.Count > 0 && map.getPath(currentUnit, walkTo, new List<Point>()).Count == 0)
-                                {
-                                    walkTo = myPath.Pop();
-                                }
-                                //System.Console.Error.WriteLine("Trying to move from " + currentUnit.position + " to " + walkTo);
-                                currentUnit.goToTile(walkTo, map);
+                                _myState = AIState.chooseTarget;
                             }
-                            _hasMoved = true;
-                            this._thinkDelay = 25;
+                            else if (currentUnit.MP > 0)
+                            {
+                                _myState = AIState.chooseMoveAway;
+                            }
+                            else
+                            {
+                                _myState = AIState.endTurn;
+                            }
                         }
-                        else if (!_hasAttacked && currentUnit.withinRange(_target))
-                        {
-                            cursor.location = _target.position;
-                            currentUnit.attack(_target);
-                            this._hasAttacked = true;
-                            this._thinkDelay = 25;
+                    break;
+                    #endregion
 
-                        }
-                        else if (_hasMoved)
+                    //TODO add functionality for going to moveAway
+                    #region chooseTarget
+                    case AIState.chooseTarget:
+                    {
+                        chooseMoveIntoRange(currentUnit, map, testUnits);
+
+                        if (chooseAttackTarget(currentUnit, map, testUnits)  && currentUnit.AP > 0)
                         {
-                            this.startNextTurn();
-                            currentUnit.isDone = true;
+                            _myState = AIState.chooseAttack;
+                        }
+                        else if (currentUnit.MP > 0)
+                        {
+                            _myState = AIState.chooseMoveTowards;
+                        }
+                        else
+                        {
+                            _myState = AIState.endTurn;
                         }
                     }
+                    break;
+                    #endregion
+
+                        //TODO Implement intelligent selection of abilities
+                    #region chooseAttack
+                    case AIState.chooseAttack:
+                    {
+                        if (chooseAttackTarget(currentUnit, map, testUnits))
+                        {
+                            _myState = AIState.attackTarget;
+                        }
+                        else
+                        {
+                            _myState = AIState.endTurn;
+                        }
+                    }
+                    break;
+                    #endregion
+
+                        //TODO implement
+                    #region chooseMoveAway
+                    case AIState.chooseMoveAway:
+                    {
+                        _myState = AIState.endTurn;
+                    }
+                    break;
+                    #endregion
+
+                        //TODO 
+                    #region chooseMoveTowards
+                    case AIState.chooseMoveTowards:
+                    {
+                        Stack<Point> fullPath = map.getPath(currentUnit, _targetPoint, new List<Point>());
+                        int cnt = currentUnit.MP;
+                        Point shortTarg = currentUnit.position;
+                        while (--cnt >= 0 && fullPath.Count > 0)
+                        {
+                            shortTarg = fullPath.Pop();
+                        }
+                        _targetPoint = shortTarg;
+
+                        if (_targetPoint != currentUnit.position)
+                        {
+                            _myState = AIState.moveToTarget;
+                        }
+                        else
+                        {
+                            _myState = AIState.endTurn;
+                        }
+
+                    }
+                    break;
+                    #endregion
+
+                        //TODO
+                    #region attackTarget
+                    case AIState.attackTarget:
+                    {
+                        currentUnit.attack(_targetUnit);
+                        if (currentUnit.AP == 0)
+                        {
+                            if (currentUnit.MP > 0)
+                            {
+                                _myState = AIState.chooseMoveAway;
+                            }
+                            else
+                            {
+                                _myState = AIState.endTurn;
+                            }
+                        }
+                        if (!_targetUnit.isDead && currentUnit.AP > 0)
+                        {
+                            _myState = AIState.chooseTarget;
+                        }
+                    }
+                    break;
+                    #endregion
+
+                        //TODO
+                    #region moveToTarget
+                    case AIState.moveToTarget:
+                    {
+                        currentUnit.goToTile(_targetPoint, map);
+
+                        //TODO delay
+
+                        _myState = AIState.chooseAttack;
+
+
+                    }
+                    break;
+                    #endregion
+
+                    //TODO
+                    #region endTurn
+                    case AIState.endTurn:
+                    {
+                        currentUnit.isDone = true;
+                        startNextTurn();
+                    }
+                    break;
+                    #endregion
+
                 }
             }
 
+            //TODO gameclock?
+            #region View Updates
             camera.update(viewWidth, viewHeight, map);
-            cursor.update(gameTime, viewWidth, viewHeight, map);
+            //cursor.update(gameTime, viewWidth, viewHeight, map);
+
             foreach (BaseUnit bu in testUnits)
             {
                 bu.update(gameTime, viewWidth, viewHeight, map);
             }
+            #endregion
+
         }
 #endregion
 
